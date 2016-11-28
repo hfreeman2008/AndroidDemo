8957e2bc3dd1521519853705b9830c75a5b0be40
xiaoming.he <xiaoming.he@tinno.com>
2016-11-28 10:52:41 +0800

    add function circle image

--- app/src/main/java/com/hxm/demo/circularimageview/CircularImageViewActivity.java
+++ app/src/main/java/com/hxm/demo/circularimageview/CircularImageViewActivity.java
@@ -1,5 +1,6 @@
 package com.hxm.demo.circularimageview;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,21 @@ import com.hxm.demo.R;
 
 public class CircularImageViewActivity extends AppCompatActivity {
 
+    private CircularImageView circularImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circular_image_view);
+        init();
+    }
+
+    private void init() {
+        circularImageView = (CircularImageView)findViewById(R.id.circularImageView);
+        circularImageView.setBorderColor(getResources().getColor(R.color.colorgreen));
+        circularImageView.setBorderWidth(5);
+        circularImageView.addShadow();
+
+
     }
 }
