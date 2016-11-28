86f3bacac9f4140c08f86f592715817f81d4b718
xiaoming.he <xiaoming.he@tinno.com>
2016-11-28 10:29:24 +0800

    add function circle image

--- app/src/main/java/com/hxm/demo/MainActivity.java
+++ app/src/main/java/com/hxm/demo/MainActivity.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.hxm.demo.circularimageview.CircularImageViewActivity;
 import com.hxm.demo.swipelistview.SwipeMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button_swip_menu_listview;
+    private Button button_circle_view;
     private Button button_test;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +31,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button_circle_view = (Button)findViewById(R.id.button_circle_view);
+        button_circle_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplication(),CircularImageViewActivity.class));
+            }
+        });
+
         button_test = (Button)findViewById(R.id.button_test);
         button_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //startActivity(new Intent(getApplication(),DifferentMenuActivity.class));
+                //startActivity(new Intent(getApplication(),CircularImageViewActivity.class));
             }
         });
     }
---
+++ app/src/main/java/com/hxm/demo/circularimageview/CircularImageViewActivity.java
@@ -0,0 +1,15 @@
+package com.hxm.demo.circularimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hxm.demo.R;
+
+public class CircularImageViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circular_image_view);
+    }
+}
